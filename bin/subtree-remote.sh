5d2f06952c5904a33fe5f885c27e5e7821649d68
Nick Maludy <nmaludy@gmail.com>
2017-09-30 12:08:19 -0400

    Added generated site subtree

--- bin/subtree-remote.sh
+++ bin/subtree-remote.sh
@@ -1,2 +1,2 @@
 git remote add subtree/themes/hugo-future-imperfect https://github.com/jpescador/hugo-future-imperfect.git
-git remote add subtree/public git@github.com:EncoreTechnologies/blog.git
+git remote add subtree/public git@github.com:EncoreTechnologies/encoretechnologies.github.io.git
